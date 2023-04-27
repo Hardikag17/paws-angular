5897ef39ab48e235e07a1df79ed769b9b818dd36
hardikag17 <hardikag17@gmail.com>
2023-04-28 00:34:35 +0530

    dobouncing implemented
    Signed-off-by: hardikag17 <hardikag17@gmail.com>

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -5,6 +5,8 @@ import { UserService } from 'src/app/services/api/user.service';
 import { StorageService } from 'src/app/services/storage.service';
 import { Pet } from 'src/app/interfaces/pet';
 import { Router } from '@angular/router';
+import { debounceTime, Subject, switchMap, distinctUntilChanged } from 'rxjs';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -14,6 +16,7 @@ export class NavbarComponent {
   user!: User;
   searchText: string = '';
   searchResults!: Pet[];
+  subject = new Subject();
   constructor(
     private getStorageService: StorageService,
     private userService: UserService,
@@ -30,24 +33,24 @@ export class NavbarComponent {
     });
   }
 
-  getPets = (searchText: string) => {
-    this.searchText = searchText;
-    if (searchText.length > 0) this.user.overlay = true;
-    else this.user.overlay = false;
-    this.petsServcie.searchPets(searchText).subscribe({
-      next: (data) => {
-        let res = data.response;
-        if (res.length > 0) {
-          this.searchResults = [];
-          this.searchResults = res;
-        }
-      },
-      error: (error) => {
-        console.error('There was an error!', error);
-      },
-    });
+  ngOnInit() {
+    this.subject
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        switchMap((searchText: any) => this.petsServcie.searchPets(searchText))
+      )
+      .subscribe((results: any) => {
+        this.searchResults = results.response;
+      });
+  }
 
-    console.warn(this.searchResults);
+  getPets = (event: any) => {
+    this.searchText = event.target.value;
+    if (this.searchText.length > 0) this.user.overlay = true;
+    else this.user.overlay = false;
+    this.subject.next(this.searchText);
+    console.warn(this.searchText);
   };
 
   Logout = () => {
