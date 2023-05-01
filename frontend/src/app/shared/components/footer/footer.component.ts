c0cc570846d0f182b8b0db2a7c421ba991cb2bc0
hardikag17 <hardikag17@gmail.com>
2023-05-01 13:07:35 +0530

    added links to footer

--- frontend/src/app/shared/components/footer/footer.component.ts
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  user!: User;
 
+  constructor(private getStorageService: StorageService) {}
+
+  ngOnInit() {
+    this.getStorageService.getUserState().subscribe({
+      next: (res) => {
+        this.user = res;
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      },
+    });
+  }
 }
